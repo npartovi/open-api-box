902abd6e70a56be7a520a688d8dc8651a7f23d12
npartovi <npartovi@gmail.com>
2022-12-04 20:47:29 -0800

    added faker

--- src/api.js
+++ src/api.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const serverless = require("serverless-http");
-// const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
+const configuration = new Configuration({
+  apiKey: "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",
+});
 
 const app = express();
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  // const openai = new OpenAIApi(configuration);
-  // const response = await openai.listModels();
+  const openai = new OpenAIApi(configuration);
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Tell me a joke about a cop and a jewish man in a bowling alley",
+    max_tokens: 64,
+    temperature: 0.4,
+  });
 
   res.json({
-    hello: `${process.env.OPENAI_API_KEY}`,
+    response: "hello",
+    data: response.data,
   });
 });
 
